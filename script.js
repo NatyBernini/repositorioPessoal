b42818af5086aac80aaec4cbffd5bfe1bbd35351
NatyBernini <110041227+NatyBernini@users.noreply.github.com>
2023-08-01 15:30:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function ativaLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        }, 75 * i);
-    });
+function escrevendoLetra(){
+    // Função Digitação
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
+
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
-ativaLetra(titulo);
+// Muda o ícone do menu para resoluções de telas menores
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria')
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
+})
+
 
